b36d9ac5a13b88ec703e29e39476f31fa0bb5b19
omnia-web <omnia.webmail@gmail.com>
2022-05-24 15:04:08 +0200

    typescript upgrade

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import '@/styles/globals.css'
-import Link from 'next/link'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import { useState, useEffect, useRef } from 'react'
 import SideBar from '@/components/Layout/SideBar/SideBar'
 import Header from '@/components/Layout/Header/Header'
 import RightSection from '@/components/Layout/RightSection/RightSection'
